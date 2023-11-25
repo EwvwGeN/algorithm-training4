339f246f004fb662db07671dbc2c89292a5ac9d9
EwvwGeN <doj54567467z@gmail.com>
2023-11-25 13:25:56 +0300

    fix: deleted forgoten files

--- homework_4/task_5/main.py
+++
@@ -1,9 +0,0 @@
-def gen(n, counter_open, counter_close, ans):
-    if counter_open + counter_close == 2 * n:
-        print(ans)
-        return ans
-    if counter_open < n:
-        gen(n, counter_open + 1, counter_close, ans + '(')
-    if counter_open > counter_close:
-        gen(n, counter_open, counter_close + 1, ans + ')')
-gen(2, 0, 0, "")
